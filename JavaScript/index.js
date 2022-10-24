a45a218851a1d2c56ed144d8c205ca53254eb069
Asghar Abbas <101616115+Abbasjafri-syed@users.noreply.github.com>
2022-10-24 23:27:10 +0500

    Javascript Basic concepts

---
+++ JavaScript/index.js
@@ -0,0 +1,30 @@
+alert('World') // create a popup 
+prompt('Hello please enter your name....') // create a pop-up for input values
+
+var Name = 'asd'; // initialising or constructing a variable needed type to be defined 
+alert(Name) // showing variable into alert
+
+
+var Level = 1;
+Level = prompt('Please enter your level....');
+
+
+
+// function to switch values;
+function main() {
+    var a = 3;
+    var b = 8;
+
+    // switching values
+    var c = b;
+    b = a;
+    a = c;
+    console.log(a);
+    console.log(b);
+}
+main() //calling function to show result
+
+
+// concatenate strings
+var named = 'Ali';
+alert('Hello ' + named);
