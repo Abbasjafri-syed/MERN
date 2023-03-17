ff84836a52c543a545767224372dddb3b7fa1ca8
Asghar Abbas <101616115+Abbasjafri-syed@users.noreply.github.com>
2023-03-17 22:21:54 +0500

    DOM in JS..

--- JavaScript/Basics/1/Document.js
+++ JavaScript/Basics/1/Document.js
@@ -6,9 +6,12 @@ document
 // give detail related to ip address
 document.location
 
+//gives domain name of the page
+document.domain
+
 // getting class element using dom
 let elm = document.getElementsByClassName('btn');
-console.log(elm);
+console.log(elm[0]);
 
 // changing attributes by selecting particular class index
 elm[0].style.backgroundColor = 'red';
@@ -19,7 +22,38 @@ elm[1].classList.add('pager'); // add class
 elm[1].classList.remove('pager'); // remove class
 
 //getting only inner text of an element at a particular index
-elm[0].innerText;
+let dc = elm[0].innerText;
+console.log(dc);
+
 
 // get all html elements
-console.log(elm[0].innerHtml);
+let dce = elm[0].innerHtml; // facing issue
+console.log(dce);
+
+//getting element by tag name
+tg = document.getElementsByTagName('meta');
+// defining particular index of tag
+console.log(tg[1]);
+
+// create an element
+let celm = document.createElement('p');
+
+// setting inner text for an element
+celm.innerText = 'This is a created paragraph.';
+
+//appending or inserting an element into an existing element
+elm[0].appendChild(celm);
+
+
+let belm = document.createElement('p');
+belm.innerText = 'This is a inserted paragraph.';
+
+// replacing an element with a new element
+elm[0].replaceChild(belm, celm); // 1st arg is new element...2nd arg is old element
+
+//query selection ..return first index of element
+q1 = document.querySelector('p'); // id  are selected with alphabetical and numeric value
+console.log(q1);
+
+q2 = document.querySelectorAll('.btn'); // id  are selected with alphabetical and numeric value
+console.log(q2);
